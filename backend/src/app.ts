7c433afc7a53d05bc5c53d30a3b1e4acaa3e953f
Rafael <rafael.constantino89@gmail.com>
2024-12-28 11:26:31 -0300

    #back feat(cors): enable cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,12 @@ import clientRoutes from "./routes/ClientRoutes";
 import appointmentRoutes from "./routes/AppointmentRoutes";
 import authRoutes from "./routes/AuthRoutes";
 import userRoutes from "./routes/UserRoutes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/clients", clientRoutes);
 app.use("/appointments", appointmentRoutes);
 app.use("/auth", authRoutes);
