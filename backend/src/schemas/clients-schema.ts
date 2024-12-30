c2a2a91cbd831bcc46fccf96660873424b8afd0f
Rafael <rafael.constantino89@gmail.com>
2024-12-29 22:19:10 -0300

    #back fix(clients): fix client schema CEP

--- backend/src/schemas/clients-schema.ts
+++ backend/src/schemas/clients-schema.ts
@@ -20,22 +20,19 @@ export const clientSchema = Joi.object({
         "com",
       ];
 
-      // Divide o nome em palavras
       const formattedName = value
         .split(" ")
         .map((part: any, index: any, array: any) => {
           const lowerPart = part.toLowerCase();
 
-          // Se for uma das palavras que devem ficar em minúsculas
           if (
             lowerCaseWords.includes(lowerPart) &&
             index !== 0 &&
             index !== array.length - 1
           ) {
-            return lowerPart; // Deixa a palavra em minúsculas, se não for a primeira ou última
+            return lowerPart;
           }
 
-          // Caso contrário, coloca a primeira letra maiúscula e o restante em minúsculas
           return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
         })
         .join(" ");
@@ -47,19 +44,18 @@ export const clientSchema = Joi.object({
   Endereço: Joi.string().trim().required(),
   Cidade: Joi.string().trim().required(),
   Estado: Joi.string().trim().required(),
+
   CEP: Joi.string()
-    .pattern(/^\d{5}-\d{3}$/) // Aceita apenas CEPs já no formato correto
-    .messages({
-      "string.pattern.base": "CEP must be in the format XXXXX-XXX.",
-    })
     .custom((value, helpers) => {
-      // Remove qualquer formatação existente e aplica o formato correto
       const digits = value.replace(/\D/g, ""); // Remove tudo que não for número
       if (digits.length !== 8) {
-        return helpers.error("CEP must contain 8 digits.");
+        return helpers.error("CEP must contain exactly 8 digits.");
       }
       return `${digits.slice(0, 5)}-${digits.slice(5)}`;
     }, "Formatador de CEP")
+    .messages({
+      "string.base": "CEP must be a valid string.",
+    })
     .required(),
 
   Telefone: Joi.string()
