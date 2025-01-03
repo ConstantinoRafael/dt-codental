fe21dc4dd29d03614d21d72f8c2d4877fa7e57f6
Rafael <rafael.constantino89@gmail.com>
2025-01-03 15:19:51 -0300

    #back fix(redix): add container host name

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,6 +1,6 @@
 import { RedisOptions } from "ioredis";
 
 export const redisConfig: RedisOptions = {
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_HOST || "redis",
   port: Number(process.env.REDIS_PORT) || 6379,
 };
